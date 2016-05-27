d9d8fbd21683c0c0961b5eaf0a47824c29f31cae
muttushingadi <muttu.singadi@gmail.com>
2016-05-27 19:10:05 +0530

    my program

---
+++ pattern1.rb
@@ -0,0 +1,20 @@
+#### 
+#program to get following pattern
+#5 7
+#9 11 13
+#15 17 19 21
+###################################
+
+
+class Padi
+
+k=5
+
+for line in 1..3
+	for count in 1..line+1
+		print k
+		k=k+2
+	end
+	puts "\n"
+end
+end
